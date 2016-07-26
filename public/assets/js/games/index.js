b758534fb316affd7c4efa1e33d1ff5e1f439794
cooperkh <cooper@fantaplan.co.kr>
2016-07-26 17:15:27 +0900

    'update'

--- public/assets/js/games/index.js
+++ public/assets/js/games/index.js
@@ -12,7 +12,6 @@ games = {
 		window.answer_caption = '';
 		window.all_voted=0;
 		FakeGameStage1();
-
 		// ViewCaptionResultPage();
 	},
 	events: function() {
@@ -45,7 +44,7 @@ request_games = {
 
 function FakeGameStage1()
 {
-	WaitingTimer(0);
+	WaitingTimer(1);
 	make_and_add_pbar_html_wt('success');
 	animate_pbar_wt();
 	setTimeout(function(){ 
@@ -79,7 +78,8 @@ function FakeGameStage2()
 {
 	// hide the waiting layer
 	$('#page-overlay-notification').fadeOut(400, function(){
-		InitiatePieTimer('#VotingTimer');
+		$('#page-overlay-notification').html('');
+		StartKnobTimer('#VotingTimer',10,0);
 	});
 	
 	//proceed to voting
@@ -154,14 +154,13 @@ function FakeGameStage3(){
 	var ht = '<center id="loading-wrapper-wt"><img class="loading-img" src="/assets/images/icons/gif/loading1.gif">'+
 		    	'<p>&nbsp;</p>'+
 		    	'<p>Game will start in</p>'+
-		    	'<p><span id="start-timer">4 Seconds</span></p>'+
+		    	'<p class="pulse"><span id="start-timer">4 Seconds</span></p>'+
 			'</center>';
 	$('#page-overlay-notification').html(ht);
 	$('#page-overlay-notification').fadeIn(400, function(){
 		$('#page-body').html('');
 		scroll_to_ele('#page-overlay-notification');
 		get_ready_timer(4);
-		prepare_game_window();
 	});
 
 }
@@ -179,6 +178,7 @@ function get_ready_timer(tm){
 function FakeGameStage4(){
 	$('.other-users-profile').css('opacity','0.5');
 	$('#page-overlay-notification').fadeOut(400, function(){
+		prepare_game_window();
 	});
 }
 function prepare_game_window(){
@@ -188,22 +188,28 @@ function prepare_game_window(){
 								'<img src = "'+chosen_one_src+'" class = "card-img img-thumbnail">'+
 							'</div>'+
 							'<div id="caption-container">'+
-								'<textarea id="caption-txtarea" placeholder="Enter caption…&#13;&#10;-or-&#13;&#10;SAY something about this picture?!?&#13;&#10;'+
+								'<textarea style="resize: none;" id="caption-txtarea" placeholder="Enter caption…&#13;&#10;-or-&#13;&#10;SAY something about this picture?!?&#13;&#10;'+
 									'" class="form-control" rows="5" id="comment"></textarea>'+
 							'</div>'+
 							'<div class="control-bar clearfix">'+
-								'<div class="timers" id="WriteCaptionTimer" data-timer="60" style="float:left;width: 100px; height: 100px;"></div>'+
-								'<div id="cb-btn-holder"><button id="sbmt-caption" class="btn btn-primary btn-sm">Submit</button></div>'+
+								'<div class="left-timer-holder">'+
+									'<input id="WriteCaptionTimer" data-role="none" data-width="100" type="text" value="0" data-linecap="round">'+
+								'</div>'+
+								'<div id="cb-btn-holder">'+
+									'<button id="sbmt-caption" class="btn btn-primary btn-sm">Submit</button>'+
+								'</div>'+
 							'</div>';
 	$('#page-body').html(caption_game_html);
-	InitiatePieTimer('#WriteCaptionTimer');
+	StartKnobTimer('#WriteCaptionTimer',35,0);
     $(document).on('click','#sbmt-caption',function(){
     	answer_caption = $('#caption-txtarea').val();
      	ViewCaptionResultPage();
     });
 }
 function ViewCaptionResultPage(){
-	var html = '<div id="captions-holder">'+
+	var html = '<h3 style="text-transform: uppercase;">Time To Vote</h3>'+
+			'<div id="rating-wrapper">'+
+				'<div id="captions-holder">'+
 					'<div class="inner-div">'+
 						'<div class="panel panel-default caption-panel">'+
 						  '<div class="panel-heading clearfix" style="text-align: left;">'+
@@ -212,7 +218,6 @@ function ViewCaptionResultPage(){
 						  			'<span id="name-text">Cooper</span>'+
 						  		'</div>'+
 						  		'<div class="heading-star">'+
-									'<input data-size="xs" class="input-7-xs"  name="input-7-xs" value="2.5" class="rating-loading" data-role="none">'+
 						  		'</div>'+
 						  	'</div>'+
 						  '</div>'+
@@ -220,7 +225,7 @@ function ViewCaptionResultPage(){
 								'<div class="form-group cap-fg">'+
 								    '<div class="input-group">'+
 								        '<div class="caption-text">'+
-								        	'<p>answer_caption</p>'+
+								        	'<p>'+answer_caption+'</p>'+
 								        '</div>'+
 								    '</div>'+
 								'</div>'+
@@ -234,10 +239,10 @@ function ViewCaptionResultPage(){
 						  '<div class="panel-heading clearfix" style="text-align: left;">'+
 						  	'<div class="heading-wrapper clearfix">'+
 						  		'<div class="heading-name">'+
-						  			'<span id="name-text">Cooper</span>'+
+						  			'<span id="name-text">Robert&nbsp;<span class="new-cap rank-cap-1"></span></span>'+
 						  		'</div>'+
 						  		'<div class="heading-star">'+
-									'<input data-size="xs" class="input-7-xs"  name="input-7-xs" value="2.5" class="rating-loading" data-role="none">'+
+									'<input id="rank-1" data-size="xs" class="input-7-xs"  name="input-7-xs" value="2.5" class="rating-loading" data-role="none">'+
 						  		'</div>'+
 						  	'</div>'+
 						  '</div>'+
@@ -245,29 +250,220 @@ function ViewCaptionResultPage(){
 								'<div class="form-group cap-fg">'+
 								    '<div class="input-group">'+
 								        '<div class="caption-text">'+
-								        	'<p>answer_caption</p>'+
+								        	'<p>Bootstrap makes front-end web development faster and easier. Its made for folks of all skill levels, devices of all shapes, and projects of all sizes.</p>'+
 								        '</div>'+
 								    '</div>'+
 								'</div>'+
 						  '</div>'+
 						'</div>'+
 					'</div>'+
-				'</div>';
+				'</div>'+
+				'<div id="captions-holder">'+
+					'<div class="inner-div">'+
+						'<div class="panel panel-default caption-panel">'+
+						  '<div class="panel-heading clearfix" style="text-align: left;">'+
+						  	'<div class="heading-wrapper clearfix">'+
+						  		'<div class="heading-name">'+
+						  			'<span id="name-text">Cooper&nbsp;<span class="new-cap rank-cap-2"></span></span>'+
+						  		'</div>'+
+						  		'<div class="heading-star">'+
+									'<input id="rank-2" data-size="xs" class="input-7-xs"  name="input-7-xs" value="2.5" class="rating-loading" data-role="none">'+
+						  		'</div>'+
+						  	'</div>'+
+						  '</div>'+
+						  '<div class="panel-body cap-body">'+
+								'<div class="form-group cap-fg">'+
+								    '<div class="input-group">'+
+								        '<div class="caption-text">'+
+								        	'<p>Bootstrap ships with vanilla CSS, but its source code utilizes the two most popular CSS preprocessors, Less and Sass. Quickly get started with precompiled CSS or build on the source.</p>'+
+								        '</div>'+
+								    '</div>'+
+								'</div>'+
+						  '</div>'+
+						'</div>'+
+					'</div>'+
+				'</div>'+
+				'<div id="captions-holder">'+
+					'<div class="inner-div">'+
+						'<div class="panel panel-default caption-panel">'+
+						  '<div class="panel-heading clearfix" style="text-align: left;">'+
+						  	'<div class="heading-wrapper clearfix">'+
+						  		'<div class="heading-name">'+
+						  			'<span id="name-text">Cooper&nbsp;<span class="new-cap rank-cap-3"></span></span>'+
+						  		'</div>'+
+						  		'<div class="heading-star">'+
+									'<input id="rank-3" data-size="xs" class="input-7-xs"  name="input-7-xs" value="2.5" class="rating-loading" data-role="none">'+
+						  		'</div>'+
+						  	'</div>'+
+						  '</div>'+
+						  '<div class="panel-body cap-body">'+
+								'<div class="form-group cap-fg">'+
+								    '<div class="input-group">'+
+								        '<div class="caption-text">'+
+								        	'<p>Millions of amazing sites across the web are being built with Bootstrap. Get started on your own with our growing collection of examples or by exploring some of our favorites.</p>'+
+								        '</div>'+
+								    '</div>'+
+								'</div>'+
+						  '</div>'+
+						'</div>'+
+					'</div>'+
+				'</div>'+
+			'</div>'+
+			'<div class="control-bar-v ">'+
+				'<div class="inner-div clearfix">'+
+				'<div class="left-timer-holder">'+
+					'<input id="RankingTimer" data-role="none" data-width="100" type="text" value="0" data-linecap="round">'+
+				'</div>'+					
+				'<div id="cb-btn-holder"><button id="see-results" class="btn btn-primary btn-sm">View Result</button></div>'+
+				'</div>'+
+			'</div>';
 	$('#page-body').html(html);
-	InitiateRating('.input-7-xs');
-	$(document).on('click','.cap-select-btn',function(){
-		$(document).find('.caption-panel').removeClass('panel-default').addClass('panel-danger');
-		$(this).parents('.caption-panel:first').removeClass('panel-danger').addClass('panel-success');
+	for (var i = 1; i <= 3; i++) {
+		var this_el = '#rank-'+i;
+		InitiateRatingVotingPage(this_el,i);
+	}
+	
+	StartKnobTimer('#RankingTimer',15,0);
+	$(document).on('click','#see-results',function(){
+		RetriveFinalRanks();
     });
 }
-function InitiateRating(elem){
-	    $(elem).rating({
-	        step: 1,
-	        starCaptions: {1: 'Very Poor', 2: 'Poor', 3: 'Ok', 4: 'Good', 5: 'Very Good'},
-	        starCaptionClasses: {1: 'text-danger', 2: 'text-warning', 3: 'text-info', 4: 'text-primary', 5: 'text-success'}
-	    });
+function RetriveFinalRanks(){
+	var html = '<div style="float: left;width: 100%;"><h3 class="pulse">Please Wait...</h3></div>';
+	$('#page-body').fadeOut(400, function(){
+		$('#page-body').html(html);
+		$('#page-body').fadeIn();
+		setTimeout(function(){ 
+			var ranks_html = 
+				'<div id="rating-wrapper">'+
+					'<div id="captions-holder">'+
+						'<div class="inner-div">'+
+							'<div class="panel panel-default caption-panel">'+
+							  '<div class="panel-heading clearfix" style="text-align: left;">'+
+							  	'<div class="heading-wrapper clearfix">'+
+							  		'<div class="heading-name">'+
+							  			'<span id="name-text">Cooper</span>'+
+							  		'</div>'+
+							  		'<div class="heading-star fadeIn" style="visibility: hidden;">'+
+										'<input data-size="xs" class="f-ranks"  name="input-7-xs" value="2.5" class="rating-loading" data-role="none">'+
+							  		'</div>'+
+							  	'</div>'+
+							  '</div>'+
+							  '<div class="panel-body cap-body">'+
+									'<div class="form-group cap-fg">'+
+									    '<div class="input-group">'+
+									        '<div class="caption-text">'+
+									        	'<p>'+answer_caption+'</p>'+
+									        '</div>'+
+									    '</div>'+
+									'</div>'+
+							  '</div>'+
+							'</div>'+
+						'</div>'+
+					'</div>'+
+					'<div id="captions-holder">'+
+						'<div class="inner-div">'+
+							'<div class="rp panel panel-default caption-panel">'+
+							  '<div class="panel-heading clearfix" style="text-align: left;">'+
+							  	'<div class="heading-wrapper clearfix">'+
+							  		'<div class="heading-name">'+
+							  			'<span id="name-text">Robert</span>'+
+							  		'</div>'+
+							  		'<div class="heading-star fadeIn" style="visibility: hidden;">'+
+										'<input data-size="xs" class="f-ranks"  name="input-7-xs" value="2.5" class="rating-loading" data-role="none">'+
+							  		'</div>'+
+							  	'</div>'+
+							  '</div>'+
+							  '<div class="panel-body cap-body">'+
+									'<div class="form-group cap-fg">'+
+									    '<div class="input-group">'+
+									        '<div class="caption-text">'+
+									        	'<p>Bootstrap makes front-end web development faster and easier. Its made for folks of all skill levels, devices of all shapes, and projects of all sizes.</p>'+
+									        '</div>'+
+									    '</div>'+
+									'</div>'+
+							  '</div>'+
+							'</div>'+
+						'</div>'+
+					'</div>'+
+					'<div id="captions-holder">'+
+						'<div class="inner-div">'+
+							'<div class="panel panel-default caption-panel">'+
+							  '<div class="panel-heading clearfix" style="text-align: left;">'+
+							  	'<div class="heading-wrapper clearfix">'+
+							  		'<div class="heading-name">'+
+							  			'<span id="name-text">Cooper</span>'+
+							  		'</div>'+
+							  		'<div class="heading-star fadeIn" style="visibility: hidden;">'+
+										'<input data-size="xs" class="f-ranks"  name="input-7-xs" value="2.5" class="rating-loading" data-role="none">'+
+							  		'</div>'+
+							  	'</div>'+
+							  '</div>'+
+							  '<div class="panel-body cap-body">'+
+									'<div class="form-group cap-fg">'+
+									    '<div class="input-group">'+
+									        '<div class="caption-text">'+
+									        	'<p>Bootstrap ships with vanilla CSS, but its source code utilizes the two most popular CSS preprocessors, Less and Sass. Quickly get started with precompiled CSS or build on the source.</p>'+
+									        '</div>'+
+									    '</div>'+
+									'</div>'+
+							  '</div>'+
+							'</div>'+
+						'</div>'+
+					'</div>'+
+					'<div id="captions-holder">'+
+						'<div class="inner-div">'+
+							'<div class="panel panel-default caption-panel">'+
+							  '<div class="panel-heading clearfix" style="text-align: left;">'+
+							  	'<div class="heading-wrapper clearfix">'+
+							  		'<div class="heading-name">'+
+							  			'<span id="name-text">Cooper</span>'+
+							  		'</div>'+
+							  		'<div class="heading-star fadeIn" style="visibility: hidden;">'+
+										'<input data-size="xs" class="f-ranks"  name="input-7-xs" value="2.5" class="rating-loading" data-role="none">'+
+							  		'</div>'+
+							  	'</div>'+
+							  '</div>'+
+							  '<div class="panel-body cap-body">'+
+									'<div class="form-group cap-fg">'+
+									    '<div class="input-group">'+
+									        '<div class="caption-text">'+
+									        	'<p>Millions of amazing sites across the web are being built with Bootstrap. Get started on your own with our growing collection of examples or by exploring some of our favorites.</p>'+
+									        '</div>'+
+									    '</div>'+
+									'</div>'+
+							  '</div>'+
+							'</div>'+
+						'</div>'+
+					'</div>'+
+				'</div>';
+			$('#page-body').fadeOut(400, function(){
+				$('#page-body').html(ranks_html);
+				InitiateRatingFinal('.f-ranks');
+				$('#page-body').fadeIn();
+				setTimeout(function(){ 
+					$(document).find('.rp').addClass('panel-big').removeClass('panel-default').addClass('panel-success');
+				 }, 1000);
+			});
+		}, 2000);
+	});
 }
-
+function InitiateRatingVotingPage(elem,_i){
+    $(elem).rating({
+    	captionElement: ".rank-cap-"+_i,
+        step: 1,
+        starCaptions: {1: 'Very Poor', 2: 'Poor', 3: 'Ok', 4: 'Good', 5: 'Very Good'},
+        starCaptionClasses: {1: 'text-danger', 2: 'text-warning', 3: 'text-info', 4: 'text-primary', 5: 'text-success'}
+    });
+}
+function InitiateRatingFinal(elem){
+    $(elem).rating({
+        step: 1,
+		displayOnly: true,
+        starCaptions: {1: 'Very Poor', 2: 'Poor', 3: 'Ok', 4: 'Good', 5: 'Very Good'},
+        starCaptionClasses: {1: 'text-danger', 2: 'text-warning', 3: 'text-info', 4: 'text-primary', 5: 'text-success'}
+    });
+}
 function InitiatePieTimer(elem){
 	$(elem).TimeCircles({ 
 		time: { 
@@ -284,3 +480,27 @@ function InitiatePieTimer(elem){
 		circle_bg_color: "#ababab", // determines the color of the background circle.
 	});
 }
+function StartKnobTimer(elem,max,min){
+	//init
+	$(elem).knob();
+	//set value
+	$(elem).val(max).trigger("change");
+	//configs
+	$(elem).trigger(
+        'configure',
+        {
+            "min":min,
+            "max":max
+        }
+	);
+	var ktimer = setInterval(function(){ 
+		max = max-1;
+		$(elem).val(max).trigger("change");
+		if (max == -1)
+		{
+		 	clearInterval(ktimer);
+		}
+	}, 1000);
+}
+
+
